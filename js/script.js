4df291f11fdad1dd26a450b15a546bb6dc5d275d
Erick Valera <valeraerick@gmail.com>
2023-11-02 16:59:16 -0300

    pop en contacto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
+// <--boton modo claro/oscuro-->
+
 const btnSwitch = document.querySelector('#switch');
 
 btnSwitch.addEventListener('click', () =>{
 document.body.classList.toggle('light');
 btnSwitch.classList.toggle('active');
 }) 
+
+//  <--popup-->
+
+let popup= document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+
+function closePopup(){
+    popup.classList.remove("open-popup");
+}
+
+
