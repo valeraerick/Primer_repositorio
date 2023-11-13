7b449842150712112d5fbb09b41e6b43ced8cc7b
Erick Valera <valeraerick@gmail.com>
2023-11-13 11:55:10 -0300

    toques finales JS

--- js/script.js
+++ js/script.js
@@ -19,4 +19,21 @@ function closePopup(){
     popup.classList.remove("open-popup");
 }
 
+// boton Scroll Top
+const btn_scrollTop = document.getElementById("btn_scrollTop")
+    btn_scrollTop.addEventListener("click", () => {
+        window.scrollTo({
+            top:0,
+            behavior: "smooth"
+        })
+
+    })
+
+window.onscroll = ()=> {
+    if (window.scrollY < 300){
+        btn_scrollTop.classList.remove("btn_scrollOn") 
+    } else {
+        btn_scrollTop.classList.add("btn_scrollOn")
+    }
+}
 
